aafc398a7e27bbd0db95c605472eafb9969ecec1
ignacio <ignaciofranciscovigo@gmail.com>
2024-10-09 23:34:59 -0300

    Refactors project structure.ç

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,19 +6,26 @@ export default [
   js.configs.recommended,
   {
     files: ["**/*.js"],
-    ignores: ["dist/**"],
     languageOptions: {
       sourceType: "commonjs",
-      globals: globals.node,
+      globals: { ...globals.node },
     },
     plugins: {
       "@stylistic/js": stylisticJs,
     },
     rules: {
       "@stylistic/js/indent": ["error", 2],
-      "@stylistic/js/linebreak-style": ["error", "unix"],
+      // "@stylistic/js/linebreak-style": ["error", "unix"],
       "@stylistic/js/quotes": ["error", "double"],
-      "@stylistic/js/semi": ["error", "never"],
+      "@stylistic/js/semi": ["error", "always"],
+      eqeqeq: "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "arrow-spacing": ["error", { before: true, after: true }],
+      "no-console": "off",
     },
+  },
+  {
+    ignores: ["dist/**", "build/**"],
   },
 ];
--- models/note.js
+++ models/note.js
@@ -1,20 +1,5 @@
 const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI;
-
-mongoose.set("strictQuery", false);
-
-console.log("Connecting to ", url);
-
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("Connected to MongoDB.");
-  })
-  .catch((error) => {
-    console.log("Error connecting to MontoDB: ", error.message);
-  });
-
 const noteSchema = mongoose.Schema({
   content: {
     type: String,
