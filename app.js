9046cda237968d7f066df40344e728b4d3af44fc
ignacio <ignaciofranciscovigo@gmail.com>
2024-11-05 23:11:26 -0300

    Adds user administration.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 // For eliminating the try catch blocks in async calls in routes
 require("express-async-errors");
 const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
@@ -28,6 +29,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
